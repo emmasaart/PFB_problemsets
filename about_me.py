ff0d340c90e349a45d73ad5d34469783c258b472
Emma Saart <esaart@northwell.edu>
2025-10-14 11:15:02 -0400

    first script

--- about_me.py
+++ about_me.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-print("My name: Emma")
+name = "Emma"
+print(f"My name: {name}")
+
+color = "Purple"
+print(f"My favorite color:{color}")
+
+activity = "Running"
+print("My favorite activity:",activity)
+
+animal = "Penguin"
+print("My favortie animal:",animal)
+
+import sys
+
+name2 = sys.argv[1]
+color2 = sys.argv[2]
+activity2 = sys.argv[3]
+animal2 = sys.argv[4]
+
+print('my name is '+ name2+ ' and my favorite color is '+ color2+ '. i like '+ activity2+ ' and '+ animal2+".")
