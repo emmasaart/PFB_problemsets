154414fcd2ae3530dffd127c819b007484741703
Emma Saart <esaart@northwell.edu>
2025-10-15 11:29:22 -0400

    ran control sequence

---
+++ python3/pset3.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+##ON POSITIVE CONTROL
+#create a varibale that stores a dna sequence
+dna = 'ATTGGGCCCC'
+#make a new variable with all uppercase
+dna_upper = dna.upper()
+#count A's 
+dna_a = dna_upper.count('A')
+#count T's
+dna_t = dna_upper.count('T')
+#count G's
+dna_g = dna_upper.count('G')
+#count C's
+dna_c = dna_upper.count('G')
+#print output
+print(f'''the sequence contains
+      {dna_a} A's
+      {dna_t} T's
+      {dna_g} G's
+      {dna_c} C's''')
+
+##ON SEQUENCE OF INTEREST
+#create a varibale that stores a dna sequence
+
+#make a new variable with all uppercase
+
+#count A's 
+
+#count T's
+
+#count G's
+
+#count C's
